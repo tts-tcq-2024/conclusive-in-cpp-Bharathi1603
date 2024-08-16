283be978da66be9c2d6baa61ea09dbb79cba2722
Bharathi1603 <bharathikec013@gmail.com>
2024-08-16 12:56:34 +0530

    Update PassiveCooling.h

--- PassiveCooling.h
+++ PassiveCooling.h
@@ -2,3 +2,17 @@
 
 #include <iostream>
 #include "CoolingStrategy.h"
+
+class PassiveCooling : public CoolingStrategy 
+{
+public:
+    int getLowerLimit() const override 
+    {
+        return 0;
+    }
+    
+    int getUpperLimit() const override 
+    {
+        return 35;
+    }
+};
