d1734f0f46f7fcf33102c1eb4cf9326ac1b139ff
Bharathi1603 <bharathikec013@gmail.com>
2024-08-16 15:48:52 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 // #include "typewise-alert.h"
-#include "TemperatureClassfier/TemperatureClassifier.h"
+#include "TemperatureClassifier/TemperatureClassifier.h"
 
 TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
 
