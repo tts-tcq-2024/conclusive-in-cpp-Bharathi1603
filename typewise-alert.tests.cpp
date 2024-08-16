fc0324e440c3e5b631a3c151fada5e477037b872
Bharathi1603 <bharathikec013@gmail.com>
2024-08-16 16:53:04 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -2,9 +2,7 @@
 // #include "typewise-alert.h"
 #include "TemperatureClassifier.h"
 
-   TemperatureClassifier classifier(CoolingType::PASSIVE);
-
-TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
-
-
+TemperatureClassifier classifier(CoolingType::PASSIVE);
+TEST(TypeWiseAlertTestSuite, PassiveCoolingBreach) {
+    EXPECT_EQ(classifier.getBreachType(-1), BreachType::TOO_LOW);
 }
