83b954503d2086274ea608e6d37a7fa9e92ad977
Bharathi1603 <bharathikec013@gmail.com>
2024-08-16 17:03:38 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -2,22 +2,25 @@
 // #include "typewise-alert.h"
 #include "TemperatureClassifier.h"
 
-TemperatureClassifier classifier(CoolingType::PASSIVE);
-TEST(TypeWiseAlertTestSuite, PassiveCoolingBreach) {
+TEST(TypeWiseAlertTestSuite, PassiveCoolingBreach) 
+{
+    TemperatureClassifier classifier(CoolingType::PASSIVE);
     EXPECT_EQ(classifier.getBreachType(-1), BreachType::TOO_LOW);
     EXPECT_EQ(classifier.getBreachType(36), BreachType::TOO_HIGH);
     EXPECT_EQ(classifier.getBreachType(30), BreachType::NORMAL);
 }
 
-classifier.setCoolingType(CoolingType::HI_ACTIVE);
-TEST(TypeWiseAlertTestSuite, HiActiveCoolingBreach) {
+TEST(TypeWiseAlertTestSuite, HiActiveCoolingBreach) 
+{
+    TemperatureClassifier classifier(CoolingType::HI_ACTIVE);
     EXPECT_EQ(classifier.getBreachType(-1), BreachType::TOO_LOW);
     EXPECT_EQ(classifier.getBreachType(46), BreachType::TOO_HIGH);
     EXPECT_EQ(classifier.getBreachType(30), BreachType::NORMAL);
 }
 
-classifier.setCoolingType(CoolingType::MED_ACTIVE);
-TEST(TypeWiseAlertTestSuite, MediumActiveCoolingBreach) {
+TEST(TypeWiseAlertTestSuite, MediumActiveCoolingBreach) 
+{
+    TemperatureClassifier classifier(CoolingType::MED_ACTIVE);
     EXPECT_EQ(classifier.getBreachType(-1), BreachType::TOO_LOW);
     EXPECT_EQ(classifier.getBreachType(41), BreachType::TOO_HIGH);
     EXPECT_EQ(classifier.getBreachType(30), BreachType::NORMAL);
