34c943d370f0e7e49382f8502deae0ba86d6938f
Bharathi1603 <bharathikec013@gmail.com>
2024-08-17 11:00:16 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -6,9 +6,9 @@
 TEST(TypeWiseAlertTestSuite, PassiveCoolingBreach) 
 {
     std::unique_ptr<CoolingContext> coolingObj = std::make_unique<CoolingContext>(std::make_unique<PassiveCooling>());
-    EXPECT_EQ(coolingObj.inferBreach(-1), BreachType::TOO_LOW);
-    EXPECT_EQ(coolingObj.inferBreach(36), BreachType::TOO_HIGH);
-    EXPECT_EQ(coolingObj.inferBreach(30), BreachType::NORMAL);
+    EXPECT_EQ(coolingObj->inferBreach(-1), BreachType::TOO_LOW);
+    EXPECT_EQ(coolingObj->inferBreach(36), BreachType::TOO_HIGH);
+    EXPECT_EQ(coolingObj->inferBreach(30), BreachType::NORMAL);
 }
 /*
 TEST(TypeWiseAlertTestSuite, HiActiveCoolingBreach) 
