a7ff600f70058e5eed4fcbc76c0b77f9221f8acc
Bharathi1603 <bharathikec013@gmail.com>
2024-08-17 12:03:13 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,29 +1,27 @@
 #include <gtest/gtest.h>
 #include "typewise-alert.h"
 
-// #include "TemperatureClassifier.h"
-
 TEST(TypeWiseAlertTestSuite, PassiveCoolingBreach) 
 {
     std::unique_ptr<CoolingContext> coolingObj = std::make_unique<CoolingContext>(std::make_unique<PassiveCooling>());
-    // EXPECT_EQ(coolingObj->inferBreach(-1), BreachType::TOO_LOW);
+    EXPECT_EQ(coolingObj->inferBreach(-1), BreachType::TOO_LOW);
     EXPECT_EQ(coolingObj->inferBreach(36), BreachType::TOO_HIGH);
     EXPECT_EQ(coolingObj->inferBreach(30), BreachType::NORMAL);
 }
-/*
+
 TEST(TypeWiseAlertTestSuite, HiActiveCoolingBreach) 
 {
-    TemperatureClassifier classifier(CoolingType::HI_ACTIVE);
-    EXPECT_EQ(classifier.getBreachType(-1), BreachType::TOO_LOW);
-    EXPECT_EQ(classifier.getBreachType(46), BreachType::TOO_HIGH);
-    EXPECT_EQ(classifier.getBreachType(30), BreachType::NORMAL);
+    std::unique_ptr<CoolingContext> coolingObj = std::make_unique<CoolingContext>(std::make_unique<HiActiveCooling>());
+    EXPECT_EQ(coolingObj->inferBreach(-1), BreachType::TOO_LOW);
+    EXPECT_EQ(coolingObj->inferBreach(46), BreachType::TOO_HIGH);
+    EXPECT_EQ(coolingObj->inferBreach(30), BreachType::NORMAL);
 }
 
 TEST(TypeWiseAlertTestSuite, MediumActiveCoolingBreach) 
 {
-    TemperatureClassifier classifier(CoolingType::MED_ACTIVE);
-    EXPECT_EQ(classifier.getBreachType(-1), BreachType::TOO_LOW);
-    EXPECT_EQ(classifier.getBreachType(41), BreachType::TOO_HIGH);
-    EXPECT_EQ(classifier.getBreachType(30), BreachType::NORMAL);
+    std::unique_ptr<CoolingContext> coolingObj = std::make_unique<CoolingContext>(std::make_unique<MedActiveCooling>());
+    EXPECT_EQ(coolingObj->inferBreach(-1), BreachType::TOO_LOW);
+    EXPECT_EQ(coolingObj->inferBreach(31), BreachType::TOO_HIGH);
+    EXPECT_EQ(coolingObj->inferBreach(30), BreachType::NORMAL);
 }
-*/
+
