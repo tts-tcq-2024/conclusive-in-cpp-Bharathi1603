8a684d852c3353d05b76cc2fbd6304136e82e1ee
Bharathi1603 <bharathikec013@gmail.com>
2024-08-17 09:25:37 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -38,20 +38,20 @@ class CoolingContext
 };
 
 
-// typedef enum {
-//   PASSIVE_COOLING,
-//   HI_ACTIVE_COOLING,
-//   MED_ACTIVE_COOLING
-// } CoolingType;
+typedef enum {
+  PASSIVE_COOLING,
+  HI_ACTIVE_COOLING,
+  MED_ACTIVE_COOLING
+} CoolingType;
 
-// typedef enum {
-//   NORMAL,
-//   TOO_LOW,
-//   TOO_HIGH
-// } BreachType;
+typedef enum {
+  NORMAL,
+  TOO_LOW,
+  TOO_HIGH
+} BreachType;
 
-// BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-// BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
 typedef enum {
   TO_CONTROLLER,
