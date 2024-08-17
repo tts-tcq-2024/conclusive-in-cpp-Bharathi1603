656f3e10f49571a7e65cc327f499b91cce8a4cf9
Bharathi1603 <bharathikec013@gmail.com>
2024-08-17 09:31:35 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -38,11 +38,11 @@ class CoolingContext
 };
 
 
-typedef enum {
-  PASSIVE_COOLING,
-  HI_ACTIVE_COOLING,
-  MED_ACTIVE_COOLING
-} CoolingType;
+// typedef enum {
+//   PASSIVE_COOLING,
+//   HI_ACTIVE_COOLING,
+//   MED_ACTIVE_COOLING
+// } CoolingType;
 
 // typedef enum {
 //   NORMAL,
@@ -50,21 +50,21 @@ typedef enum {
 //   TOO_HIGH
 // } BreachType;
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+// BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+// BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
-typedef enum {
-  TO_CONTROLLER,
-  TO_EMAIL
-} AlertTarget;
+// typedef enum {
+//   TO_CONTROLLER,
+//   TO_EMAIL
+// } AlertTarget;
 
-typedef struct {
-  CoolingType coolingType;
-  char brand[48];
-} BatteryCharacter;
+// typedef struct {
+//   CoolingType coolingType;
+//   char brand[48];
+// } BatteryCharacter;
 
-void checkAndAlert(
-  AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
+// void checkAndAlert(
+//   AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 
-void sendToController(BreachType breachType);
-void sendToEmail(BreachType breachType);
+// void sendToController(BreachType breachType);
+// void sendToEmail(BreachType breachType);
