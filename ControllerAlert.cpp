d9d7221a1f4db93b9981461172c478b7b6f27ef7
Bharathi1603 <bharathikec013@gmail.com>
2024-08-17 12:22:55 +0530

    Delete ControllerAlert.cpp

--- ControllerAlert.cpp
+++
@@ -1,7 +0,0 @@
-#include "ControllerAlert.h"
-
-void ControllerAlert::send(BreachType breachType) const 
-{
-    const unsigned short header = 0xfeed;
-    std::cout << std::hex << header << " : " << static_cast<std::uint16_t>(breachType) << std::endl;
-}
